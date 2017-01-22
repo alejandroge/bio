d12d85fb344b710367f84c6c26f187822971bff6
Alejandro <alejandro.guevara.esc@gmail.com>
2017-01-22 11:51:18 -0600

    Se añade la estructure de archivos

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <meta charset="utf-8">
     <title>Home</title>
   </head>
   <body>
+
+    <header>
+      <div class="container">
+        <h1 class="">Alejandro Guevara</h1>
+        <nav class="">
+          <ul>
+            <li>Bio</li>
+            <li>Blog</li>
+            <li>Home</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    
 
   </body>
 </html>
