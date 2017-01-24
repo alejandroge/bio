f6014790a418d3669333b1cb7035be55d57750d3
Alejandro <alejandro.guevara.esc@gmail.com>
2017-01-23 21:58:28 -0600

    Se añadió el header a las páginas principales

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300" rel="stylesheet">
     <meta charset="utf-8">
     <title>Home</title>
   </head>
@@ -14,18 +12,16 @@
 
     <header>
       <div class="container">
-        <h1 class="">Alejandro Guevara</h1>
-        <nav class="">
+        <h1 class="logo col-md-8">Alejandro Guevara</h1>
+        <nav class="col-md-4">
           <ul>
-            <li>Bio</li>
-            <li>Blog</li>
-            <li>Home</li>
+            <li><a href="bio.php">Bio</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="index.php">Home</a></li>
           </ul>
         </nav>
       </div>
     </header>
-
-    
 
   </body>
 </html>
